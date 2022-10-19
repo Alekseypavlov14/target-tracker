ff685f059ae95e46730994c6f12b246b80580441
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-10-19 20:08:44 +0300

    add new path in Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Layout } from './components/Layout/Layout'
 import { TopicsPage } from './features/topics/components/TopicsPage/TopicsPage'
 import { SingleTopicPage } from './features/topics/components/SingleTopicPage/SingleTopicPage'
+import { AddTargetPage } from './features/targets/components/AddTargetPage/AddTargetPage'
 import './App.css'
 
 export function App() {
@@ -11,6 +12,7 @@ export function App() {
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<TopicsPage />} />
           <Route path='/topic/:id' element={<SingleTopicPage />} />
+          <Route path='/topic/:id/add' element={<AddTargetPage /> } />
         </Route>
       </Routes>
     </div>
