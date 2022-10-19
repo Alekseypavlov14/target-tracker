1ce843d5041544c892ab66fe55f1a09fa80f356d
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-10-19 20:11:20 +0300

    add hook to find all targets of topic

---
+++ src/features/targets/hooks/useTargets.ts
@@ -0,0 +1,8 @@
+import { useSelector } from "react-redux"
+import { targetsSelector } from "../slice/targets.slice"
+
+export function useTargets(topicId: number) {
+  const targets = useSelector(targetsSelector)
+  const topicsTargets = targets.filter(target => target.id === topicId)
+  return topicsTargets
+}
