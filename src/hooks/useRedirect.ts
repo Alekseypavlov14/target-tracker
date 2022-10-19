7875532fc433357a011d47667360eac1b04c5b14
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-10-19 20:08:21 +0300

    add shared hook

---
+++ src/hooks/useRedirect.ts
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { topicsSelector } from '../features/topics/slice/topic.slice'
+
+export function useRedirect(id: number, redirectPath: string = '/') {
+  const navigate = useNavigate()
+  const topics = useSelector(topicsSelector)
+  
+  useEffect(() => {
+    if (Number.isNaN(id)) navigate(redirectPath)
+
+    const topic = topics.find(topic => topic.id === id)
+    
+    if (!topic) navigate(redirectPath)
+  }, [id, redirectPath, navigate, topics])
+}
